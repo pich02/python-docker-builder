2061963c6f8b26c8f0fdecb0131433aac4d77309
pich02 <pich.2@hotmail.fr>
2024-04-24 18:10:27 +0200

    fix bash

--- create_symlink.sh
+++ create_symlink.sh
@@ -2,6 +2,8 @@
 
 if [[ ${TARGETARCH} == "amd64" ]]; then
   ARCH="x86_64"
+elif [[ ${TARGETARCH} == "arm64"  ]]
+ ARCH="arm"
 else
   ARCH=${TARGETARCH}
 fi
@@ -11,3 +13,5 @@ echo "SYMLINK ARCH : $ARCH"
 ln -s /usr/local/include/soplex/ /scip-9.0.0/lib/include/spxinc;
 ln -s /usr/local/lib/libsoplex.a /scip-9.0.0/lib/static/libsoplex.linux.${ARCH}.gnu.a;
 ln -s /usr/local/lib/libsoplex.a /usr/local/lib/libsoplex.linux.${ARCH}.gnu.opt.a;
+
+ldconfig -v;
